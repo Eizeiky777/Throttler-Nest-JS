4704676b5980a50ff997920295a042f0b1a9c0e0
Eizeiky <eizeiky@lidowin.com>
2022-08-15 15:09:46 +0700

    rate limiter nest

--- src/public/public.controller.ts
+++ src/public/public.controller.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import {
   Controller,
   Get,
@@ -12,17 +13,24 @@ import { PublicService } from './public.service';
 import { CreatePublicDto } from './dto/create-public.dto';
 import { UpdatePublicDto } from './dto/update-public.dto';
 import { ThrottlerBehindProxyGuard } from 'src/guards/throttler/throttler';
+import { SkipThrottle, Throttle, ThrottlerGuard } from '@nestjs/throttler';
+import { ConfigService } from 'src/config/config.service';
 
 @Controller('public')
 export class PublicController {
-  constructor(private readonly publicService: PublicService) {}
+  constructor(
+    private readonly publicService: PublicService,
+    private readonly configService: ConfigService,
+  ) {}
 
   @Post()
   create(@Body() createPublicDto: CreatePublicDto) {
     return this.publicService.create(createPublicDto);
   }
 
-  @UseGuards(ThrottlerBehindProxyGuard)
+  @SkipThrottle(false)
+  @UseGuards(ThrottlerGuard)
+  @Throttle(5, 2)
   @Get()
   findAll() {
     return this.publicService.findAll();
--- src/public/public.module.ts
+++ src/public/public.module.ts
@@ -2,12 +2,15 @@ import { Module } from '@nestjs/common';
 import { PublicService } from './public.service';
 import { PublicController } from './public.controller';
 import { ThrottlerStorageRedisService } from 'nestjs-throttler-storage-redis';
-import { ThrottlerModule } from '@nestjs/throttler';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
 import { ConfigService } from 'src/config/config.service';
+import { APP_GUARD } from '@nestjs/core';
+import { ConfigModule } from 'src/modules/config.module';
 
 @Module({
   imports: [
     ThrottlerModule.forRootAsync({
+      imports: [ConfigModule],
       inject: [ConfigService],
       useFactory: (config: ConfigService) => ({
         ttl: Number(config.get('THROTTLE_TTL')),
@@ -19,7 +22,13 @@ import { ConfigService } from 'src/config/config.service';
     }),
   ],
   controllers: [PublicController],
-  providers: [PublicService],
+  providers: [
+    PublicService,
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+  ],
   exports: [PublicService],
 })
 export class PublicModule {}
